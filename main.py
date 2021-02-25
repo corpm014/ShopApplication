e64ee2b3cbf74307076d7e90dd314c7d2b778b1f
MarcCorpuz <corpm014@richardchalloner.com>
2021-02-25 12:28:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import sqlite3
+
+
+"""
+
+I would like someone to work on database.py (this can be found on the file section on the right)
+
+Inside GUI folder will be the GUI needed and will be displayed on the screen - We need people to work on this as this will be the harderst part
+
+# Link
+https://github.com/
+
+# 
+https://github.com/corpm014
+
+YOU ARE PART OF THIS PRODUCTION TEAM SO DO NOT USE GITHUB AGAINST THE SCHOOL's RULES!
+
+"""
+
+
+# Run line
+if __name__ == "__main__":
+    # Do something
+    sys.exit(0)
